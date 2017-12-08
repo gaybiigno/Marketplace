6ce7bcff98e208804d86656db94a9ce64c0cf154
gaybiigno <igno@sonoma.edu>
2017-12-07 20:34:41 -0800

    Inbox alert thing for delete!

--- Marketplace/InboxMsgTableCell.swift
+++ Marketplace/InboxMsgTableCell.swift
@@ -10,18 +10,29 @@ import UIKit
 
 class InboxMsgTableCell: UITableViewCell {
 
-	@IBOutlet weak var itemImage: UIImageView!
-	@IBOutlet weak var itemTitle: UILabel!
+	@IBOutlet weak var msgSubject: UILabel!
 	@IBOutlet weak var otherUserName: UILabel!
 	@IBOutlet weak var msgIcon: UIImageView!
 	
 	 //var thisMessage: Message? = nil
 	var originalCenter = CGPoint()
 	var deleteOnDragRelease = false
+	var originalFrame = CGRect()
 	
 	override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+		
+//		let deleteHold = UILongPressGestureRecognizer(target: self, action: #selector(deleteThat(_:)))
+//		deleteHold.minimumPressDuration = 1.0
+//		deleteHold.delaysTouchesBegan = true
+//		deleteHold.delegate = self
+//		self.addGestureRecognizer(deleteHold)
+		
+		let deleteGesture = UIPanGestureRecognizer(target: self, action: #selector(deleteThread(_:)))
+		deleteGesture.delegate = self
+		self.addGestureRecognizer(deleteGesture)
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,28 +42,18 @@ class InboxMsgTableCell: UITableViewCell {
     }
 	
 	func useMessage(_ img: UIImage, _ subject: String, _ otherUser: String, _ iconShown: Bool) {
-		itemImage.image = img
-		itemTitle.text = subject
+		//itemImage.image = img
+		msgSubject.text = subject
 		otherUserName.text = otherUser
 		msgIcon.isHidden = !iconShown
 	}
 	
-//	override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-//		super.init(style: style, reuseIdentifier: reuseIdentifier)
-//
-//		// add a pan recognizer
-//		var recognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-//		recognizer.delegate = self
-//		addGestureRecognizer(recognizer)
-//	}
-//
-//	required init?(coder aDecoder: NSCoder) {
-//		fatalError("init(coder:) has not been implemented")
-//	}
+	@objc func deleteThat(_ sender: UILongPressGestureRecognizer) {
+		print("Long pressed dat bish")
+		popUp()
+	}
 	
-	//MARK: - horizontal pan gesture methods
-	@objc func handlePan(_ recognizer: UIPanGestureRecognizer) {
-		// 1
+	@objc func deleteThread(_ recognizer: UIPanGestureRecognizer) {
 		if recognizer.state == .began {
 			// when the gesture begins, record the current center location
 			originalCenter = center
@@ -61,43 +62,59 @@ class InboxMsgTableCell: UITableViewCell {
 		if recognizer.state == .changed {
 			let translation = recognizer.translation(in: self)
 			center = CGPoint(x: originalCenter.x + translation.x, y: originalCenter.y)
+			print("center now is: \(center)")
+			
 			// has the user dragged the item far enough to initiate a delete/complete?
-			deleteOnDragRelease = frame.origin.x < -frame.size.width / 2.0
+			//recognizer.setTranslation(center, in: self)
+			deleteOnDragRelease = center.x < originalCenter.x - 100.0
+			//deleteOnDragRelease = frame.origin.x < frame.size.width / 2.0
+			//print("in changed")
 		}
 		// 3
 		if recognizer.state == .ended {
+			print("in ended")
 			// the frame this cell had before user dragged it
-			let originalFrame = CGRect(x: 0, y: frame.origin.y,
+			originalFrame = CGRect(x: 0, y: frame.origin.y,
 			                           width: bounds.size.width, height: bounds.size.height)
 			if !deleteOnDragRelease {
 				// if the item is not being deleted, snap back to the original location
-				UIView.animate(withDuration: 0.2, animations: {self.frame = originalFrame})
+				UIView.animate(withDuration: 0.2, animations: {self.frame = self.originalFrame})
+			} else {
+				popUp()
 			}
 		}
+		
+		
+		
+		
 	}
 	
-//	override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
-//		if let panGestureRecognizer = gestureRecognizer as? UIPanGestureRecognizer {
-//			let translation = panGestureRecognizer.translation(in: superview!)
-//			if fabs(translation.x) > fabs(translation.y) {
-//				return true
-//			}
-//			return false
-//		}
-//		return false
-//	}
+	func popUp() {
+		let alertController = UIAlertController(title: "Confirm Delete", message: "Are you sure you'd like to permanently delete this message thread?", preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "Delete", style: UIAlertActionStyle.default) {
+			UIAlertAction in
+			self.deleteMessage()
+			UIView.animate(withDuration: 0.2, animations: {self.frame = self.originalFrame})
+		})
+		alertController.addAction(UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel) {
+			UIAlertAction in
+			UIView.animate(withDuration: 0.2, animations: {self.frame = self.originalFrame})
+		})
+		UIApplication.shared.keyWindow?.rootViewController?.present(alertController, animated: true, completion: nil)
+	}
 	
-	// Check if user is recipient or sender
-//	func checkRole(_ recipient: String, _ sender: String) {
-//
-//	}
+	
+	
+	func deleteMessage() {
+		print("Message deleted!")
+	}
+	
 	
 	/*
 	func useMessage(_ message: Message?) {
 		thisMessage = message
 		if let m = message {
-			itemTitle.text = message?.subject
-			otherUserName = message?.item_name
+			msgSubject.text = message?.subject
 			cellPrice.text = String (describing: message?.price)
 		} else {
 			cellLabel.text = "Item not found!"
